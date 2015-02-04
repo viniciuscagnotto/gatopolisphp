ac842b2b6db63431d7e67a4f0488a305a270b6d9
Vinicius Cagnotto <viniciuscagnotto@gmail.com>
2015-02-04 11:21:24 -0500

    Testing DB Connection
    Simple Db Connection and SELECT statement

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php 
 
-print "Hello World";
+$host = "br-cdbr-azure-south-a.cloudapp.net";
+$user = "b33f81945fc541";
+$pwd = "a2536739";
+$db = "gatopolisphpdb";
+
+$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+$sql_select = "SELECT * FROM user_test";
+$stmt = $conn->query($sql_select);
+$registrants = $stmt->fetchAll();
+if(count($registrants) > 0) {
+	foreach($registrants as $registrant) {
+		echo $registrant['name']."<br />";
+	}
+}
+
 
 ?>
