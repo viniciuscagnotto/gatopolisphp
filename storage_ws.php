7d7de7a189501341c70ca3a2ecd1bfebe165c978
Vinicius Cagnotto <viniciuscagnotto@gmail.com>
2015-03-27 00:31:16 -0300

    changing container

--- storage_ws.php
+++ storage_ws.php
@@ -5,19 +5,19 @@ use WindowsAzure\Common\ServicesBuilder;
 
 class StorageWS {
  
-        public function Save($containerToSave, $stringToSave) {
+        public function Save($stringToSave) {
         	$connectionString = "DefaultEndpointsProtocol=https;AccountName=gatopolisphpstorage;AccountKey=MKz0r3FP4329Qk6opMqUh5T64GpchgTs1HYwIdPeKfmC0mxrfA75Q11HW5p7SeXQ2CBodAMF1ZEVDDbVSmgMEg==";
         	$blob_name = str_replace(" ", "", "blob_".microtime());
         	$blob_name = str_replace(".", "", $blob_name);
         	$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
-        	$blobRestProxy->createBlockBlob($containerToSave, $blob_name, $stringToSave);
+        	$blobRestProxy->createBlockBlob("gatopolis", $blob_name, $stringToSave);
         	return $blob_name;
         }
         
-        public function Get($containerToGetFrom, $blobToString) {
+        public function Get($blobToString) {
         	$connectionString = "DefaultEndpointsProtocol=https;AccountName=gatopolisphpstorage;AccountKey=MKz0r3FP4329Qk6opMqUh5T64GpchgTs1HYwIdPeKfmC0mxrfA75Q11HW5p7SeXQ2CBodAMF1ZEVDDbVSmgMEg==";
         	$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
-        	$blob = $blobRestProxy->getBlob($containerToGetFrom, $blobToString);
+        	$blob = $blobRestProxy->getBlob("gatopolis", $blobToString);
         	return stream_get_contents($blob->getContentStream());
         }
        
